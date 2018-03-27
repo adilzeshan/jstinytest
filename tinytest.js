6be8d853f9768c919e283852b01e7ca4dd6a79af
Adil Zeshan <adil@zeshan.org>
2018-03-27 23:24:53 +0100

    Remove unnecessary use of bind()

--- tinytest.js
+++ tinytest.js
@@ -83,9 +83,9 @@ var TinyTest = {
 
 };
 
-var fail               = TinyTest.fail.bind(TinyTest),
-    assert             = TinyTest.assert.bind(TinyTest),
-    assertEquals       = TinyTest.assertEquals.bind(TinyTest),
-    eq                 = TinyTest.assertEquals.bind(TinyTest), // alias for assertEquals
-    assertStrictEquals = TinyTest.assertStrictEquals.bind(TinyTest),
-    tests              = TinyTest.run.bind(TinyTest);
+var fail               = TinyTest.fail,
+    assert             = TinyTest.assert,
+    assertEquals       = TinyTest.assertEquals,
+    eq                 = TinyTest.assertEquals, // alias for assertEquals
+    assertStrictEquals = TinyTest.assertStrictEquals,
+    tests              = TinyTest.run;
